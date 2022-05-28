1bf3bf2958d5bd5d7959d68943778c2b01c175b0
Quinn Hodges <qhodges0442@gmail.com>
2022-05-27 21:40:40 -0400

    roboteq test with up down arrows

--- roboteq_test/src/main.rs
+++ roboteq_test/src/main.rs
@@ -1,13 +1,41 @@
 use relay::can_extentions::prelude::*;
 use socketcan::CANSocket;
+use std::sync::mpsc::channel;
+use std::sync::{Arc, Mutex};
+use termion::input::TermRead;
+use termion::event::Key;
+use termion::raw::IntoRawMode;
 
 fn main() {
+    let throttle_percent = Arc::new(Mutex::new(0));
     let node_id = 0;
     let max_motors = 1;
-    let throttle_percent = 100;
+    let mut stdout = std::io::stdout().into_raw_mode();
+    // let throttle_percent = 100;
     let socket = CANSocket::open("can0").unwrap();
+
+    let throttle_percent_2 = throttle_percent.clone();
+
+    std::thread::spawn(move || {
+        let stdin = std::io::stdin();
+        for c in stdin.keys() {
+            let mut throttle_percent = throttle_percent_2.lock().unwrap();
+            match c.unwrap() {
+                Key::Up => { *throttle_percent += 1 },
+                Key::Down => { *throttle_percent -= 1 }
+                Key::Ctrl('c') => { panic!(); }
+                _ => {}
+            }
+        }
+    });
+
+
     loop {
-        socket.set_motor_throttle(node_id, max_motors, throttle_percent).expect("To Send the message");
+        {
+            let throttle_percent = throttle_percent.lock().unwrap();
+            socket.set_motor_throttle(node_id, max_motors, *throttle_percent).expect("To Send the message");
+            println!("Throttle percent {:?}\n\r", throttle_percent);
+        }
         std::thread::sleep(std::time::Duration::from_millis(50));
     }
     println!("Hello, world!");
